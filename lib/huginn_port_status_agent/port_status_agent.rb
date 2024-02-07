83937285b2d11153107596074feb0e5662a7b0c9
Nicolas Germain <ngermain@hihouhou.com>
2024-02-07 23:30:44 +0100

    GHA added and port list to scan too

--- lib/huginn_port_status_agent/port_status_agent.rb
+++ lib/huginn_port_status_agent/port_status_agent.rb
@@ -9,6 +9,16 @@ module Agents
       <<-MD
       The agent checks a port (TCP) for a specific host.
 
+      `target` is the wanted host you want to scan.
+
+      `debug` is used for verbose mode.
+
+      `timeout` is the delay to validate the status for a port.
+
+      `min_port` is the starting point for the list of ports to be scanned.
+
+      `max_port` is the end of the list of ports to be scanned.
+
       `expected_receive_period_in_days` is used to determine if the Agent is working. Set it to the maximum number of days
       that you anticipate passing without this Agent receiving an incoming Event.
       MD
@@ -26,27 +36,39 @@ module Agents
 
     def default_options
       {
-        'ip' => '',
-        'port' => '',
+        'target' => '',
+        'min_port' => '1',
+        'max_port' => '1024',
+        'debug' => 'false',
         'expected_receive_period_in_days' => '2',
         'timeout_sec' => '1',
         'changes_only' => 'true'
       }
     end
 
+    form_configurable :debug, type: :boolean
     form_configurable :expected_receive_period_in_days, type: :string
-    form_configurable :ip, type: :string
-    form_configurable :port, type: :string
+    form_configurable :target, type: :string
+    form_configurable :min_port, type: :number
+    form_configurable :max_port, type: :number
     form_configurable :timeout_sec, type: :string
     form_configurable :changes_only, type: :boolean
 
     def validate_options
-      unless options['ip'].present?
-        errors.add(:base, "ip is a required field")
+      unless options['target'].present?
+        errors.add(:base, "target is a required field")
       end
 
-      unless options['port'].present?
-        errors.add(:base, "port is a required field")
+      unless options['min_port'].present?
+        errors.add(:base, "min_port is a required field")
+      end
+
+      unless options['max_port'].present?
+        errors.add(:base, "max_port is a required field")
+      end
+
+      if options['min_port'] > options['max_port']
+        errors.add(:base, "min_port must be lower than max_port")
       end
 
       if options.has_key?('changes_only') && boolify(options['changes_only']).nil?
@@ -55,6 +77,10 @@ module Agents
 
       unless options['timeout_sec'].present?
         errors.add(:base, "timeout_sec is a required field")
+      end
+
+      if options.has_key?('debug') && boolify(options['debug']).nil?
+        errors.add(:base, "if provided, debug must be true or false")
       end
 
       unless options['expected_receive_period_in_days'].present? && options['expected_receive_period_in_days'].to_i > 0
@@ -67,37 +93,77 @@ module Agents
     end
 
     def check
-      port_open? interpolated['ip'], interpolated[:port].to_i, interpolated[:timeout_sec].to_i
+      fetch
     end
 
     private
 
-    def port_open?(ip, port, seconds)
-      Timeout::timeout(seconds) do
-        begin
-          TCPSocket.new(ip, port).close
-          port_status = 'opened'
-          log "port #{port} opened on #{ip}"
-        rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
-          log "port #{port} closed on #{ip}"
-          port_status = 'closed'
-        end
-        payload = { 'ip' => "#{ip}", 'port' => "#{port}", 'status' => "#{port_status}" }
+    def scanport(port)
+            s = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
+            begin
+                    sockaddr = Socket.pack_sockaddr_in(port, interpolated['target'])
+            rescue
+                    puts "[!] Error: Failed to Resolve Target"
+                    exit
+            end
+            Timeout::timeout(interpolated['timeout']) do
+                    begin
+                            @result = s.connect(sockaddr)
+                            return port
+                    rescue
+                            return false
+                    end
+            end
+    end
 
-        if interpolated['changes_only'] == 'true'
-          if payload.to_s != memory['last_status']
-            memory['last_status'] = payload.to_s
-            create_event payload: payload
-          end
-        else
-          create_event payload: payload
-          if payload.to_s != memory['last_status']
-            memory['last_status'] = payload.to_s
-          end
+    def fetch()
+
+      ports_info = {}
+      to_scan = ((interpolated['min_port'])..(interpolated['max_port'])).to_a
+      if interpolated['debug'] == 'true'
+        log "port's list to scan"
+        log to_scan
+      end
+      to_scan.each do |port|
+        if result = scanport(port)
+          ports_info[result] = "open"
         end
       end
-    rescue Timeout::Error
-      false
+
+      final_result = {'ports' => ports_info}
+      if final_result != memory['last_status']
+        final_result["ports"].each do |port, status|
+          found = false
+          if !memory['last_status'].nil? and memory['last_status'].present?
+            last_status = memory['last_status']
+            if interpolated['debug'] == 'true'
+              log "last_status"
+              log last_status
+            end
+            last_status["ports"].each do |portbis, statusbis|
+              if port == portbis && status == statusbis
+                found = true
+                if interpolated['debug'] == 'true'
+                  log "found is #{found}"
+                end
+              end
+            end
+          end
+          if found == false
+            create_event payload: { 'target' => interpolated['target'], 'port' => port, 'status' => status }
+          else
+            if interpolated['debug'] == 'true'
+              log "found is #{found}"
+            end
+          end
+        end
+        memory['last_status'] = final_result
+      else
+        if interpolated['debug'] == 'true'
+          log "no diff"
+        end
+      end
+
     end
   end
 end
